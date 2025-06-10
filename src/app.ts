6209e61235ab165cc1fe2e9cc23a94c8771b4db4
Mohammed-Shaaban-Ali <ms7500746@gmail.com>
2025-06-10 10:04:06 +0300

    fix: simplify CORS middleware configuration

--- src/app.ts
+++ src/app.ts
@@ -23,13 +23,7 @@ const app: Application = express();
 
 // Middlewares
 app.use(express.json());
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(cors());
 
 // TESTING API
 configurePassport(passport);
